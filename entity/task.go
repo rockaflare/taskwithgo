711c6a4d485a6edb1bfbb70ce7fc69320e2070ba
rockaflare <raziqin.ninetynine@gmail.com>
2022-08-10 00:39:43 +0800

    change task struct

--- entity/task.go
+++ entity/task.go
@@ -1,8 +1,11 @@
 package entity
 
+import "time"
+
 type Task struct {
-	ID      int    `json:"id"`
-	Pegawai string `json:"pegawai"`
-	Detail  string `json:"detail"`
-	IsDone  string `json:"isdone"`
+	ID       int       `json:"id"`
+	Pegawai  string    `json:"pegawai"`
+	Detail   string    `json:"detail"`
+	Deadline time.Time `json:"deadline"`
+	IsDone   bool      `json:"isdone"`
 }
